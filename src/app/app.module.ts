188786abc7885505d26698bdd323d5021ec4f103
Sriram <sriramvaikundaraman@gmail.com>
2020-07-05 12:47:15 +0530

    feedback added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -56,7 +59,10 @@ import { ContactComponent } from './contact/contact.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/shared/feedback.ts
@@ -0,0 +1,11 @@
+export class Feedback{
+    firstname:String;
+    lastname:String;
+    telnum:String;
+    email:String;
+    agree:boolean;
+    contacttype:String;
+    message:String;
+}
+
+export const ContactType=['None','Tel','Email'];
